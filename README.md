2783e846c567d3850df908e25f102d2bbc1d95c0
tamaina <tamaina@hotmail.co.jp>
2025-07-18 09:58:45 +0900

    use 54334

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # postgres-size-test
+
+## How to run tests
+docker compose up -d
+
+## Suites
+### note.ts
+
+### uri.ts
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
     "db": {
         "host": "127.0.0.1",
-        "port": 54312,
+        "port": 54334,
         "db": "test",
         "user": "postgres",
         "password": "postgres"
